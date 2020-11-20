f224a7003afcec430adac6b18cb6e6602fa66982
Jack Burgess <j.burgess3@ncl.ac.uk>
2020-11-20 15:03:32 +0000

    Implemented countdown timer command

--- cogs/basic_cog.py
+++ cogs/basic_cog.py
@@ -1,5 +1,9 @@
 from discord.ext import commands
 
+from datetime import datetime
+
+from math import floor
+
 
 class Basic(commands.Cog):
 
@@ -7,3 +11,20 @@ class Basic(commands.Cog):
     async def hello(self, ctx):
         await ctx.send(f"Hello {ctx.author.mention}")
 
+    @commands.command(name='countdown')
+    async def countdown(self, ctx, unit='days'):
+        viking_date = datetime(2021, 11, 19, 18, 00, 00)
+        current_date = datetime.now()
+
+        countdown = viking_date - current_date
+
+        if unit == "days":
+            await ctx.send(f"There are {countdown.days} more day(s) until Viking Rally 2021")
+        elif unit == "weeks":
+            await ctx.send(f"There are {countdown.days / 7} more week(s) until Viking Rally 2021")
+        elif unit == "hours":
+            await ctx.send(f"There are {floor(countdown.total_seconds() / 3600)} more hour(s) until Viking Rally 2021")
+        elif unit == "minutes":
+            await ctx.send(f"There are {floor(countdown.total_seconds() / 60)} more minutes(s) until Viking Rally 2021")
+        elif unit == "seconds":
+            await ctx.send(f"There are {floor(countdown.total_seconds())} more seconds(s) until Viking Rally 2021")
