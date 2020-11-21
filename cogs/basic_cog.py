77ef84246ff03da453ff46dcf384c5f6688bc88a
Jack Burgess <j.burgess3@ncl.ac.uk>
2020-11-21 10:15:21 +0000

    Modified Grammar

--- cogs/basic_cog.py
+++ cogs/basic_cog.py
@@ -37,7 +37,7 @@ class Basic(commands.Cog):
         embed.add_field(name="When is it?", value="19th - 21st November 2021", inline=False)
         embed.add_field(name="Where is it?", value="Moor House Adventure Centre, Durham", inline=False)
         embed.add_field(name="Who's running it?", value="NUSSAGG and DUSAGG", inline=False)
-        embed.add_field(name="What makes it so special?", value="Its the first SSAGO Rally in the North East for 21 Years!", inline=False)
+        embed.add_field(name="What makes it so special?", value="It's the first SSAGO Rally in the North East for 21 Years!", inline=False)
         embed.set_footer(text="For more information about the Rally, click on 'Viking Rally 2021'", icon_url="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/p960x960/126024485_189553636113350_3374549951797900365_o.png?_nc_cat=106&ccb=2&_nc_sid=85a577&_nc_ohc=AHTAX0o4YIAAX-xICmR&_nc_ht=scontent-lht6-1.xx&oh=708c1ec3789fb4ee98886e9d6cc0a998&oe=5FDC9301")
 
         await ctx.send(embed=embed)
