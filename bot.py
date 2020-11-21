342713a08b31b5084dfd209e3f8a76895daffc74
Jack Burgess <j.burgess3@ncl.ac.uk>
2020-11-21 15:35:09 +0000

    Added error 'handling' (logging)

--- bot.py
+++ bot.py
@@ -4,7 +4,14 @@ from cogs import basic_cog, translate_cog
 
 import os
 from dotenv import load_dotenv
+import logging
 
+# Set up error logger
+logger = logging.getLogger('Discord')
+logger.setLevel(logging.ERROR)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='a')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 class Bjorn(commands.Bot):
 
@@ -15,9 +22,15 @@ class Bjorn(commands.Bot):
         self.add_cog(basic_cog.Basic())
         self.add_cog(translate_cog.Translate())
 
+    # Error Handling (In the loosest sense of the word). Errors get sent to log
+    async def on_command_error(self, ctx, exception):
+        await ctx.send('Å nei!, somethings gone wrong here. I\'ve sent more info about what\'s gone wrong to my developers so they can work this out')
+        logger.error(exception)
 
+# Load discord token from .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# Start Bjorn Running
 bjorn = Bjorn()
 bjorn.run(TOKEN)
