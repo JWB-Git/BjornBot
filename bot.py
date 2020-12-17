5b96b598c3f2435c01211c4bec41df5a8ac48163
Jack Burgess <j.burgess3@ncl.ac.uk>
2020-12-17 12:17:47 +0000

    Basic birthday functionality (which required addition of firebase wrapper library)

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 from discord.ext import commands
 
-from cogs import basic_cog, translate_cog
+from cogs import basic_cog, translate_cog, birthday_cog
 
 import os
 from dotenv import load_dotenv
 import logging
+
+import pyrebase
 
 # Set up error logger
 logger = logging.getLogger('Discord')
@@ -18,14 +20,28 @@ class Bjorn(commands.Bot):
     def __init__(self):
         commands.Bot.__init__(self, command_prefix=commands.when_mentioned_or("Bjørn "), case_insensitive = True)
 
+        #Set up Firebase Database
+        config = {
+            'apiKey': "AIzaSyD3o48rC-dUgSwGlO9vmgy9fJnZ1gVii2E",
+            'authDomain': "bjornbot-21a44.firebaseapp.com",
+            'storageBucket': "bjornbot-21a44.appspot.com",
+            'databaseURL': 'https://bjornbot-21a44-default-rtdb.europe-west1.firebasedatabase.app/'
+        }
+        firebase = pyrebase.initialize_app(config)
+        database = firebase.database()
+
         # Add cogs here
         self.add_cog(basic_cog.Basic())
         self.add_cog(translate_cog.Translate())
+        self.add_cog(birthday_cog.Birthday(database))
 
     # Error Handling (In the loosest sense of the word). Errors get sent to log
     async def on_command_error(self, ctx, exception):
-        await ctx.send('Å nei!, somethings gone wrong here. I\'ve sent more info about what\'s gone wrong to my developers so they can work this out')
-        logger.error(exception)
+        if isinstance(exception, commands.errors.MissingRequiredArgument):  # Specific error capture for missing argument requirement
+            await ctx.send('Ah Nej! You\'ve missed a required argument for this command! This command will have its own help argument, so type it again with help after it!')
+        else:
+            await ctx.send('Å nei!, somethings gone wrong here. I\'ve sent more info about what\'s gone wrong to my developers so they can work this out')
+            logger.error(exception)
 
 # Load discord token from .env file
 load_dotenv()
---
+++ cogs/birthday_cog.py
@@ -0,0 +1,52 @@
+from discord.ext import commands
+from discord import Embed, Color
+
+from datetime import datetime
+
+import math
+
+class Birthday(commands.Cog):
+
+    def __init__(self, database):
+        self.database = database
+
+    @commands.command(name='birthday')
+    async def birthday(self, ctx, case):
+        if case == 'help':
+            await self.help(ctx)
+        elif case == 'read':
+            await self.read(ctx)
+
+    async def help(self, ctx):
+        pass
+
+    async def read(self, ctx):
+        birthdays = self.database.child('birthdays').get()
+
+        for birthday in birthdays.each():
+            dateVal = birthday.val()['birthday']
+
+            dateObj = datetime.strptime(dateVal, '%d/%m/%Y')
+            today = datetime.now()
+            age = today.year - dateObj.year
+            if today.month < dateObj.month or (today.month == dateObj.month and today.day < dateObj.day):
+                age -= 1
+
+            description = birthday.val()['description']
+
+
+
+            embed = Embed(title=birthday.key(), color=Color.purple())
+            embed.add_field(name="Birth date", value=dateVal, inline=True)
+            embed.add_field(name="Current Age", value=age, inline=True)
+            embed.add_field(name="Who", value=description, inline=False)
+            embed.set_footer(text="Viking Rally 2021 - 19th to 21st November 2021 @ Moorhouse Adventure Centre, Durham",
+                             icon_url="https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/p960x960/126024485_189553636113350_3374549951797900365_o.png?_nc_cat=106&ccb=2&_nc_sid=85a577&_nc_ohc=AHTAX0o4YIAAX-xICmR&_nc_ht=scontent-lht6-1.xx&oh=708c1ec3789fb4ee98886e9d6cc0a998&oe=5FDC9301")
+
+            await ctx.send(embed=embed)
+
+
+
+
+
+
