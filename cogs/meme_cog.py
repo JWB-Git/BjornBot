20c222f2fee4d7867caf4903127ab1e6b1465f9e
trodaway <tim.rodaway@btinternet.com>
2020-12-18 22:10:44 +0000

    added meme command, which posts a random meme from r/vikingmemes

--- cogs/meme_cog.py
+++ cogs/meme_cog.py
@@ -3,6 +3,9 @@ import discord
 import aiohttp
 import io
 import os
+import praw
+import random
+from dotenv import load_dotenv
 
 
 class Meme(commands.Cog):
@@ -30,3 +33,26 @@ class Meme(commands.Cog):
                                 data = io.BytesIO(await resp.read())
                                 channel = self.bot.get_channel(689401725005725709)  # SSAGO meme channel
                                 await channel.send(file=discord.File(data, os.path.basename(attachment.url)))
+
+    @commands.command(name="meme")
+    async def meme(self, ctx):
+        await ctx.message.add_reaction("<viking:779801546002661447>")
+        load_dotenv()
+        reddit_id = os.getenv("REDDIT_ID")
+        reddit_secret = os.getenv("REDDIT_SECRET")
+        reddit_useragent = os.getenv("REDDIT_USERAGENT")
+        reddit = praw.Reddit(client_id=reddit_id, client_secret=reddit_secret, user_agent=reddit_useragent)
+        post_to_pick = random.randint(1, len(list(reddit.subreddit('vikingmemes').hot())))
+        posts = reddit.subreddit('vikingmemes').hot()
+        for i in range(0, post_to_pick):
+            submission = next(post for post in posts if not post.stickied)
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(submission.url) as resp:
+                    if resp.status != 200:
+                        return await ctx.send("Whoops, something's gone wrong...")
+                    data = io.BytesIO(await resp.read())
+                    await ctx.send(f"{submission.title}, courtesy of u/{submission.author}",
+                                   file=discord.File(data, os.path.basename(submission.url)))
+        except Exception as e:
+            print(e)
